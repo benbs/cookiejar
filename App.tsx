a3b0e8023ce84e16af8557effd6101d997d7264f
Ben <ben@storemaven.com>
2019-11-19 14:16:30 +0200

    resolved merge conflicts with Elia

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
-import SnackPage from './src/components/SnackPage';
-import SnackList from './src/components/SnackList';
-import BasicFlatList from './src/components/common/BasicFlatList'
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack'
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
- return(
-<SnackList />
- );
-}
+const MainNavigator = createStackNavigator({
+  Home: { screen: HomeScreen }
+});
 
+export const App = createAppContainer(MainNavigator);
+export default App;
